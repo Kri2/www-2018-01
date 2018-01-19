c970ccb9c6d0c3d23e069051a9385d8ba5467a1e
Krzysztof Turlej <chris_peachtree@yahoo.com>
2018-01-19 15:27:56 +0100

    wip, added browserSync, and others

--- gulpfile.js
+++ gulpfile.js
@@ -4,25 +4,39 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer')
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+browserSync = require('browser-sync').create();
 
 gulp.task('default',function(){
     console.log("Hooray, You created a gulp task!");
 });
+
 gulp.task('html',function(){
     console.log("Imagine something useful being done to your HTML here.");
 });
+
 gulp.task('styles',function(){
     console.log("Imagine Sass or PostCSS tasks running here.");
     return gulp.src("./app/assets/styles/styles.css")
-    .pipe(postcss([cssImport,cssvars,nested,autoprefixer]))
-    .pipe(gulp.dest('./app/temp/styles'));//return jest dlatego, ze src pipe jest asynchronious, node needs to know when it finishes
+        .pipe(postcss([cssImport,cssvars,nested,autoprefixer]))
+        .pipe(gulp.dest('./app/temp/styles'));//return jest dlatego, ze src pipe jest asynchronious, node needs to know when it finishes
 });
+
 gulp.task('watch',function(){
+    browserSync.init({
+        server:{
+            //notify: false,//when we don't want a black box when reloading
+            baseDir:"app"
+        }
+    });
     watch('./app/index.html', function(){
-        gulp.start('html');
+        browserSync.reload();//gulp.start('html');//was just an example now is for reloading
     });
-    watch('./app/assets/**/*.css', function(){
-        gulp.start('styles');
+    watch('./app/assets/styles/**/*.css', function(){
+        gulp.start('cssInject');
     });
+});
+
+gulp.task('cssInject',['styles'],function(){
+    return gulp.src('./app/temp/styles/styles.css').pipe(browserSync.stream());
 });
